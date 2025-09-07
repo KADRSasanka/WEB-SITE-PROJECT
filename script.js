25757302677be39b8d9802f0392289ed4ebc620b
Ravindu Gunarathne <adrsgunarathne@students.nsbm.ac.lk>
2025-09-07 19:18:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let searchForm = document.querySelector('.search-form');
+let searchBtn = document.querySelector('#search-btn');
+    // Toggle search form when clicking the search icon
+searchBtn.onclick = (e) => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+let shoppingCart = document.querySelector('.shopping-cart');
+let cartBtn = document.querySelector('#cart-btn');
+    // Toggle cart form when clicking the cart icon
+cartBtn.onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+let loginForm = document.querySelector('.login-form');
+let loginBtn = document.querySelector('#login-btn');
+    // Toggle login form when clicking the cart icon
+loginBtn.onclick = (e) => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+let navbar = document.querySelector('ul');
+let menuBtn = document.querySelector('#btn');
+    // Toggle login form when clicking the cart icon
+menuBtn.onclick = (e) => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
